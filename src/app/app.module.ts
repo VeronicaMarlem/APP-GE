faf10fbc6ba9168f7a098943df60807dc6add4d6
Vero <vcarrillo@grupoeducare.com>
2019-02-18 15:12:09 -0600

    Modificación en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-// rutas
+// RUTAS
 import { APP_ROUTES } from './app.routes';
 
+// MODULOS
+import { PaginasModule } from './paginas/paginas.module';
+
+// COMPONENTES
 import { AppComponent } from './app.component';
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
 import { RegistroComponent } from './inicio-sesion/registro.component';
-import { NopagencontradaComponent } from './compartido/nopagencontrada/nopagencontrada.component';
-import { PrincipalComponent } from './paginas/principal/principal.component';
-import { HeaderComponent } from './compartido/header/header.component';
-import { SidebarComponent } from './compartido/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './compartido/breadcrumbs/breadcrumbs.component';
-import { PaginasComponent } from './paginas/paginas.component';
-
 
 
 @NgModule({
@@ -22,17 +19,12 @@ import { PaginasComponent } from './paginas/paginas.component';
     AppComponent,
     InicioSesionComponent,
     RegistroComponent,
-    NopagencontradaComponent,
-    PrincipalComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PaginasComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PaginasModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
